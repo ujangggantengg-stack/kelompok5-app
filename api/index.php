7dba65112bb2afd10969d7ebdffa636a0c4b7c5b
fakhri12d <msi.katana15.corei7@outlook.com>
2026-06-05 22:51:09 +0700

    Catch all fatal errors

--- api/index.php
+++ api/index.php
@@ -3,14 +3,21 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
-define('LARAVEL_START', microtime(true));
+try {
+    define('LARAVEL_START', microtime(true));
 
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
+    if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+        require $maintenance;
+    }
+
+    require __DIR__.'/../vendor/autoload.php';
+
+    $app = require_once __DIR__.'/../bootstrap/app.php';
+
+    $app->handleRequest(Request::capture());
+} catch (\Throwable $e) {
+    echo "<h1>FATAL ERROR CAUGHT IN INDEX.PHP</h1>";
+    echo "<p>" . $e->getMessage() . "</p>";
+    echo "<pre>" . $e->getTraceAsString() . "</pre>";
+    echo "<p>File: " . $e->getFile() . " on line " . $e->getLine() . "</p>";
 }
-
-require __DIR__.'/../vendor/autoload.php';
-
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-$app->handleRequest(Request::capture());
